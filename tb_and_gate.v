5967ae6fc2d6158b3e26a7a8c1a72bfcc444a44e
Hanumanthu suresh <sureshhanumanthu73@gmail.com>
2026-02-07 14:25:35 +0530

    Create tb_and_gate.v

---
+++ tb_and_gate.v
@@ -0,0 +1,17 @@
+module tb;
+    reg a, b;
+    wire y;
+
+    and_gate uut (.a(a), .b(b), .y(y));
+
+    initial begin
+        $dumpfile("dump.vcd");
+        $dumpvars(0, tb);
+
+        a=0; b=0;
+        #10 a=0; b=1;
+        #10 a=1; b=0;
+        #10 a=1; b=1;
+        #10 $finish;
+    end
+endmodule
